7d9c4fa3812230f8e003f382c182626a9906ed1b
Luka Pečnik <lukapecnik96@gmail.com>
2020-12-05 12:11:08 +0100

    remove 10-fold cross validation, remove boxplot export

--- niaaml/tests/test_utilities.py
+++ niaaml/tests/test_utilities.py
@@ -33,25 +33,13 @@ class OptimizationStatsTestCase(TestCase):
        'Class 2', 'Class 2', 'Class 1', 'Class 2', 'Class 1', 'Class 2',
        'Class 2', 'Class 2'])
 
-        self.__stats = OptimizationStats(predicted, y, np.array([0.88, 0.9, 0.91, 0.87, 0.7, 0.98, 0.95, 0.86, 0.88, 0.76]), 'Accuracy')
+        self.__stats = OptimizationStats(predicted, y)
 
     def test_works_fine(self):
         self.assertEqual(self.__stats._accuracy, 0.5)
         self.assertEqual(self.__stats._precision, 0.5199999999999999)
         self.assertEqual(self.__stats._cohen_kappa, 0.0)
         self.assertEqual(self.__stats._f1_score, 0.505050505050505)
-        self.assertTrue((np.array([0.88, 0.9, 0.91, 0.87, 0.7, 0.98, 0.95, 0.86, 0.88, 0.76]) == self.__stats._fitness_function_values).all())
-        self.assertEqual(self.__stats._fitness_function_name, 'Accuracy')
-
-    def test_export_works_fine(self):
-        with tempfile.TemporaryDirectory() as tmp:
-            self.__stats.export_boxplot(os.path.join(tmp, 'boxplot'))
-            self.assertTrue(os.path.exists(os.path.join(tmp, 'boxplot.png')))
-            self.assertEqual(1, len([name for name in os.listdir(tmp)]))
-
-            self.__stats.export_boxplot(os.path.join(tmp, 'boxplot.png'))
-            self.assertTrue(os.path.exists(os.path.join(tmp, 'boxplot.png')))
-            self.assertEqual(1, len([name for name in os.listdir(tmp)]))
 
 class MinMaxTestCase(TestCase):
     def test_works_fine(self):
