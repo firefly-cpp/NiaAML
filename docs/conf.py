1965b8255ab5ed3e585a87088a9f25bc21d8c01a
lukapecnik <lukapecnik96@gmail.com>
2021-06-27 15:21:56 +0200

    Update version

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2020, Luka Pečnik"
 author = "Luka Pečnik"
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.5"
+release = "1.1.6"
 
 
 # -- General configuration ---------------------------------------------------
