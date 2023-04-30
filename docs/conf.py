12feffb97985b2180ee7a74b28b91336c60b301f
firefly-cpp <iztok@iztok-jr-fister.eu>
2023-04-30 13:17:46 +0200

    Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -21,11 +21,11 @@ sys.path.insert(0, os.path.abspath("../"))
 # -- Project information -----------------------------------------------------
 
 project = "NiaAML"
-copyright = "2020-2022, Luka Pečnik"
+copyright = "2020-2023, Luka Pečnik"
 author = "Luka Pečnik"
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.11"
+release = "1.1.12"
 
 
 # -- General configuration ---------------------------------------------------
